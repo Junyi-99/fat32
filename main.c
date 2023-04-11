830197220b544f100381230c5501e55027e62ba4
JUNYI HOU <hji200914@gmail.com>
2023-04-11 20:20:05 +0000

    finish sc

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <fcntl.h>
 #include <linux/limits.h>
 #include <stdbool.h>
@@ -9,70 +10,71 @@
 
 #include "fat.h"
 
-/*
- * Check if the given disk image is a FAT32 disk image.
- * Return true if it is, false otherwise.
- *
- * WARNING: THIS FUNCTION IS FOR DEMONSTRATION PURPOSES ONLY!
- * This function uses an external command called "file" to check the file
- * system, but your program shouldn't rely on outside programs. You need to
- * create your own function to check the file system type and delete this
- * function before submitting. If you use this function in your submission,
- * you'll get a penalty.
- *
- * RTFM: Section 3.5
- */
-bool check_fat32(const char *disk) {
-#warning "You must remove this function before submitting."
-    char buf[PATH_MAX];
-    FILE *proc;
+int RootDirSectors = 0; // the count of sectors occupied by the root directory
+int DataSec = 0;        // the count of sectors in the data region of the volume
+int FatsSecCnt = 0;
+int MAX = -1;
+int fat_type = -1;
 
-    snprintf(buf, PATH_MAX, "file %s", disk);
-    proc = popen(buf, "r");
-    if (proc == NULL) {
-        perror("popen");
-        exit(1);
+void determin_entry_for_cluster(int cluster_number_N) {
+    if (fat_type == 16 || fat_type == 32) {
+
     }
-    fread(buf, 1, PATH_MAX, proc);
-    return strstr(buf, "FAT (32 bit)") != NULL;
 }
 
-/*
- * Hexdump the given data.
- *
- * WARNING: THIS FUNCTION IS ONLY FOR DEBUGGING PURPOSES!
- * This function prints the data using an external command called "hexdump", but
- * your program should not depend on external programs. Before submitting, you
- * must remove this function. If you include this function in your submission,
- * you may face a penalty.
- */
-void hexdump(const void *data, size_t size) {
-#warning "You must remove this function before submitting."
-    FILE *proc;
+int check_fat_type(const struct BPB *hdr) {
+    int FATSz = 0;
+    int TotSec = 0;
 
-    proc = popen("hexdump -C", "w");
-    if (proc == NULL) {
-        perror("popen");
-        exit(1);
+    RootDirSectors = ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec - 1)) /
+                     hdr->BPB_BytsPerSec;
+
+    if (hdr->BPB_FATSz16 != 0)
+        FATSz = hdr->BPB_FATSz16;
+    else
+        FATSz = hdr->fat32.BPB_FATSz32;
+
+    if (hdr->BPB_TotSec16 != 0)
+        TotSec = hdr->BPB_TotSec16;
+    else
+        TotSec = hdr->BPB_TotSec32;
+    FatsSecCnt = hdr->BPB_NumFATs * FATSz;
+    DataSec = TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) +
+                        RootDirSectors);
+
+    int CountofClusters = DataSec / hdr->BPB_SecPerClus;
+    MAX = CountofClusters + 1;
+    if (CountofClusters < 4085) {
+        assert(CountofClusters >= 0);
+        return 12;
+    } else if (CountofClusters < 65525) {
+        return 16;
+    } else {
+        assert(RootDirSectors == 0);
+        assert(hdr->BPB_RootEntCnt == 0);
+        return 32;
     }
-    fwrite(data, 1, size, proc);
+
+    return -1;
 }
 
 int main(int argc, char *argv[]) {
     setbuf(stdout, NULL);
     if (argc < 3) {
         fprintf(stderr, "Usage: %s disk.img ck\n", argv[0]);
+        fprintf(stderr, "       %s disk.img ls\n", argv[0]);
+        fprintf(stderr,
+                "       %s disk.img cp image:/path/to/source "
+                "local:/path/to/destination\n",
+                argv[0]);
+        fprintf(stderr, "       %s disk.img cp /path/to/be/remove\n", argv[0]);
+        fprintf(stderr,
+                "       %s disk.img cp local:/path/to/source "
+                "image:/path/to/destination\n",
+                argv[0]);
         exit(1);
     }
     const char *diskimg = argv[1];
-
-    /*
-     * This demo program only works on FAT32 disk images.
-     */
-    if (!check_fat32(argv[1])) {
-        fprintf(stderr, "%s is not a FAT32 disk image\n", diskimg);
-        exit(1);
-    }
 
     /*
      * Open the disk image and map it to memory.
@@ -104,18 +106,32 @@ int main(int argc, char *argv[]) {
      * Print some information about the disk image.
      */
     const struct BPB *hdr = (const struct BPB *)image;
-    printf("FAT%d filesystem\n", 32);
-    printf("BytsPerSec = %u\n", hdr->BPB_BytsPerSec);
-    printf("SecPerClus = %u\n", hdr->BPB_SecPerClus);
-    printf("RsvdSecCnt = %u\n", hdr->BPB_RsvdSecCnt);
-    printf("FATsSecCnt = ?\n");
-    printf("RootSecCnt = ?\n");
-    printf("DataSecCnt = ?\n");
+    fat_type = check_fat_type(hdr);
+    if (fat_type == -1) {
+        fprintf(stderr, "%s is not a FAT12/FAT16/FAT32 disk image\n", diskimg);
+        exit(1);
+    }
+
+    if (strncmp(argv[2], "ck", 2) == 0) {
+        printf("FAT%d filesystem\n", fat_type);
+        printf("BytsPerSec = %u\n", hdr->BPB_BytsPerSec);
+        printf("SecPerClus = %u\n", hdr->BPB_SecPerClus);
+        printf("RsvdSecCnt = %u\n", hdr->BPB_RsvdSecCnt);
+        printf("FATsSecCnt = %u\n", FatsSecCnt); // TODO: check FatsSecCnt
+        printf("RootSecCnt = %u\n", RootDirSectors);
+        printf("DataSecCnt = %u\n", DataSec);
+
+    } else if (strncmp(argv[2], "ls", 2) == 0) {
+
+    } else if (strncmp(argv[2], "cp", 2) == 0) {
+        
+    }
+    // TODO: Support backup sector
 
     /*
      * Print the contents of the first cluster.
      */
-    hexdump(image, sizeof(*hdr));
+    // hexdump(image, sizeof(*hdr));
 
     munmap(image, size);
 }
